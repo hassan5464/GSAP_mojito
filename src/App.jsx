2f64c8c44bab7986475492c710fd4e16bfc22fd0
hassan5464 <hassanelmonge034@gmail.com>
2025-08-08 18:11:43 +0300

    edit to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import gsap from "gsap";
 import{ScrollTrigger, SplitText} from "gsap/all"
-import Cocktails from './components/cocktails';
+import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
 import Menu from './components/Menu';
